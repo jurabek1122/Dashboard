06f8309e15f9be83c0a54c8b151563e866b450bc
Abdulboriy <84060501+Abdulboriydeveloper@users.noreply.github.com>
2022-12-14 10:39:57 +0500

    Page uploaded

---
+++ src/pages/FreePage.jsx
@@ -0,0 +1,11 @@
+import React { useState, useEffect } from 'react'
+const FreePage = () => {
+    
+    return(
+        <div className='add-student'>
+            <h1>Page Qo`shildi</h1>
+        </div>
+    )
+}
+
+export default FreePage;
